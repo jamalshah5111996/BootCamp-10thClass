cd5d683e4fac8d667303e17068e4a040215f6aed
jamalshah5111996 <jamalshah5111996@gmail.com>
2020-12-30 20:41:40 +0500

    2nd Comit After Api

--- src/components/infoPanel.js
+++ src/components/infoPanel.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginTop: '20px',
+    marginTop: "20px",
   },
   paper: {
     padding: theme.spacing(2),
-    textAlign: 'center',
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
+  title:{
+    color: '#3F51B5',
+    textTransform: 'uppercase',
+  }
 }));
 
 export default function InfoPanel() {
+  const [globalData, setGlobalData] = useState({});
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://api.covid19api.com/summary");
+      let data = await response.json();
+      setGlobalData(data.Global);
+      console.log(data.Global);
+    }
+    getData();
+  }, []);
+
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper}>Grid 3</Paper>
-        </Grid>
+        {Object.keys(globalData).map((key, index) => {
+          return (
+            <Grid item xs={12} sm={4} key={index}>
+              <Paper className={classes.paper} elevation={5}>
+                <h3 className={classes.title}>{key}</h3>
+                <h3>{globalData[key]}</h3>
+              </Paper>
+            </Grid>
+          );
+        })}
       </Grid>
     </div>
   );
